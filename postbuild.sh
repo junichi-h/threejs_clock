9cb13264a534aac93fe40a486c7002f1221a9bb4
Junichi Honda <junichi.hnd@gmail.com>
2017-04-28 12:39:17 +0900

    title周りを修正

--- postbuild.sh
+++ postbuild.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 #img
-cp -R ./app/assets ./dist/jpn/events/mobilmo
-cp -R ./app/assets ./dist/events/mobilmo
-
-rm -R ./dist/jpn/events/mobilmo/assets/img/sprites
-rm -R ./dist/events/mobilmo/assets/img/sprites
 
 #js
-cp ./.tmp/scripts/webgl-index.bundle.js ./dist/jpn/events/mobilmo/scripts/
-cp ./.tmp/scripts/webgl-index.bundle.js ./dist/events/mobilmo/scripts/
